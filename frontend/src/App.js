c04c870160fcd2fb840b0560c8a91f5d834986ff
Claire List <claire.list2501@gmail.com>
2021-12-20 14:01:41 -0700

    architecture ? built for sanity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom"; //this is the needful for linking stuff. 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Potluck Planner
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      </header> */}
+      {/*LINKS!*/}
+      <Link to = '/login'>Log me IN!</Link>
+      <Link to = '/logout'>Log me OUT!</Link>
+      <Link to = '/create-event'>Create an EVENT!</Link>
+
+      {/* <PrivateRoute exact path='/view' component={CreateEvent} />
+        <Route exact path="/" component={Login} />
+      <PrivateRoute exact path='/logout' component={Logout} />
+        <Route path='/login' component={Login} /> */}
     </div>
   );
 }
