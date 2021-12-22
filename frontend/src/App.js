49e6eb6b042d659a3878f40f5cb7bd385fbc2eb6
Hailey Magoline <magoline.hailey@gmail.com>
2021-12-22 10:34:33 -0600

    Added css styling to create event, upcoming events, and navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,21 +9,78 @@ import CreateEvent from './Components/CreateEvent';
 import Home from './Components/Home'
 import UpcomingEvents from './Components/UpcomingEvents'
 
+import styled from 'styled-components'
+
+const HeaderDiv = styled.div`
+  display: flex;
+  width: 100%;
+  font-family: 'Quicksand', sans-serif;
+  text-shadow: 1px 1px 2px black, 0 0 100px black, 0 0 5px darkblue;
+
+header{
+  display: flex;
+  width: 100%;
+  flex-flow: column wrap;
+  justify-content: center;
+}
+
+div{
+  width: 100%;
+  text-align: center;
+}
+
+h1{
+  font-size: 3rem;
+  width: 100%;
+  /* font-family: 'Cantarell', sans-serif; */
+  font-family: 'Codystar', cursive;
+   /* font-family: 'Courgette', cursive; */
+   /* font-family: 'Lato', sans-serif; */
+  /* font-family: 'Yellowtail', cursive; */
+/* font-family: 'Codystar', cursive;
+font-family: 'Courgette', cursive;
+font-family: 'Lato', sans-serif;
+font-family: 'Yellowtail', cursive; */
+text-shadow: 1px 1px 2px black, 0 0 25px #750D37, 0 0 5px #0f46bd;
+}
+
+//1D84B5
+
+  & .navLink{
+    margin: 1%;
+    text-decoration: none; 
+    color: white;
+    font-size: 1.4rem;
+  }
+
+`
+
+const AppDiv = styled.div`
+  /* max-width: 100%;
+  max-height: 100%; */
+`
+
 const App = () => { 
 return (
 
-	<div>
+	<AppDiv>
 
+  <HeaderDiv>
   	<header>
-    	<h1>Potluck Planner</h1>
+      <div>
+        <h1>Potluck Planner</h1>
+      </div>
+    	<div>
       	<nav>
-        	<Link to="/">Home</Link>
-          <Link to="/CreateEvent">Create Event</Link>
-          <Link to="/UpcomingEvents">Upcoming Events</Link>
-          <Link to = '/login'>Login</Link>
-          <Link to = '/logout'>Logout</Link>
+        	<Link className='navLink' to="/">Home</Link>
+          <Link className='navLink' to="/CreateEvent">Create Event</Link>
+          <Link className='navLink' to="/UpcomingEvents">Upcoming Events</Link>
+          <Link className='navLink' to = '/login'>Login</Link>
+          <Link className='navLink' to = '/logout'>Logout</Link>
         </nav>
+      </div>  
     </header>
+   </HeaderDiv>
 
     <Switch>
   
@@ -44,7 +101,7 @@ return (
         </Route>
 
     </Switch>
-  </div>
+  </AppDiv>
 	)
 
 }
--- frontend/src/components/Events.js
+++
@@ -1,9 +0,0 @@
-import React, {useState,useEffect} from 'react';
-
-export default Events=(props)=>{
-    return (
-        <div className='create-event-container'>
-            <h1>Skeleton of Events !</h1>
-        </div>
-    )
-}
